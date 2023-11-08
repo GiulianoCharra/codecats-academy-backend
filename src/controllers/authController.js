21aac8a4465d569b898d842356446bda15f46c07
GiulianoCharra <65138966+GiulianoCharra@users.noreply.github.com>
2023-11-08 03:05:40 -0300

    agregado el access control header login

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -46,6 +46,7 @@ export async function login(req, res) {
     });
     res
       .header("authorization", `Bearer ${token}`)
+      .header.add("Access-Control-Expose-Headers", "Authorization")
       .status(200)
       .json({ message: "User logged successfully" });
   } catch (error) {
